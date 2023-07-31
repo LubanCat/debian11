61f99f6776678c2a040602256fd3aca9c84736bb
hejiawencc <hjw0415@outlook.com>
2023-07-31 13:59:47 +0800

    base 升级base镜像构建脚本

--- mk-base-debian.sh
+++ mk-base-debian.sh
@@ -1,13 +1,47 @@
 #!/bin/bash -e
 
-if [ "$RELEASE" == "stretch" ]; then
+if [ ! $TARGET ]; then
+	echo "---------------------------------------------------------"
+	echo "please enter desktop type number:"
+	echo "请输入要构建桌面的序号:"
+	echo "[0] Exit Menu"
+	echo "[1] xfce"
+	echo "[2] lxde"
+	echo "[3] gnome"
+	echo "[4] lite"
+	echo "---------------------------------------------------------"
+	read input
+
+	case $input in
+		0)
+			exit;;
+		1)
+			TARGET=xfce
+			;;
+		2)
+			TARGET=lxde
+			;;
+		3)
+			TARGET=gnome
+			;;
+		4)
+			TARGET=lite
+			;;
+		*)
+			echo 'input desktop type error, exit !'
+			exit;;
+	esac
+fi
+
+if [[ "$RELEASE" == "stretch" || "$RELEASE" == "9" ]]; then
 	RELEASE='stretch'
-elif [ "$RELEASE" == "buster" ]; then
+elif [[ "$RELEASE" == "buster" || "$RELEASE" == "10" ]]; then
 	RELEASE='buster'
-elif [ "$RELEASE" == "bullseye" ]; then
+elif [[ "$RELEASE" == "bullseye" || "$RELEASE" == "11" ]]; then
 	RELEASE='bullseye'
 else
-    echo -e "\033[36m please input the os type,stretch, buster or bullseye...... \033[0m"
+	RELEASE='bullseye'
+	echo -e "\033[47;36m set default RELEASE=bullseye...... \033[0m"
 fi
 
 if [ "$ARCH" == "armhf" ]; then
@@ -15,20 +49,18 @@ if [ "$ARCH" == "armhf" ]; then
 elif [ "$ARCH" == "arm64" ]; then
 	ARCH='arm64'
 else
-    echo -e "\033[36m please input the os type,armhf or arm64...... \033[0m"
+	ARCH="arm64"
+	echo -e "\033[47;36m set default ARCH=arm64...... \033[0m"
 fi
 
-if [ ! $TARGET ]; then
-	TARGET='desktop'
+if [ -e linaro-$RELEASE-$TARGET-alip-*.tar.gz ]; then
+	rm linaro-$RELEASE-$TARGET-alip-*.tar.gz
 fi
 
-if [ -e linaro-$RELEASE-alip-*.tar.gz ]; then
-	rm linaro-$RELEASE-alip-*.tar.gz
-fi
 
 cd ubuntu-build-service/$RELEASE-$TARGET-$ARCH
 
-echo -e "\033[36m Staring Download...... \033[0m"
+echo -e "\033[47;36m Staring Download...... \033[0m"
 
 make clean
 
@@ -36,9 +68,10 @@ make clean
 
 make
 
+DATE=$(date +%Y%m%d)
 if [ -e linaro-$RELEASE-alip-*.tar.gz ]; then
 	sudo chmod 0666 linaro-$RELEASE-alip-*.tar.gz
-	mv linaro-$RELEASE-alip-*.tar.gz ../../
+	mv linaro-$RELEASE-alip-*.tar.gz ../../linaro-$RELEASE-$TARGET-alip-$DATE.tar.gz
 else
-	echo -e "\e[31m Failed to run livebuild, please check your network connection. \e[0m"
+	echo -e "\e[41;31m Failed to run livebuild, please check your network connection. \e[0m"
 fi
