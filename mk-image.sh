3544825ac41187f66a122c9bd403a42da9daef3b
hejiawencc <hjw0415@outlook.com>
2023-08-01 11:38:46 +0800

    rootfs 更新打包脚本

--- mk-image.sh
+++ mk-image.sh
@@ -2,7 +2,7 @@
 
 TARGET_ROOTFS_DIR=./binary
 ROOTFSIMAGE=linaro-$IMAGE_VERSION-rootfs.img
-EXTRA_SIZE_MB=100
+EXTRA_SIZE_MB=200
 IMAGE_SIZE_MB=$(( $(sudo du -sh -m ${TARGET_ROOTFS_DIR} | cut -f1) + ${EXTRA_SIZE_MB} ))
 
 
@@ -12,10 +12,12 @@ if [ -e ${ROOTFSIMAGE} ]; then
 	rm ${ROOTFSIMAGE}
 fi
 
-for script in ./post-build.sh ../device/rockchip/common/post-build.sh; do
-	[ -x $script ] || continue
-	sudo $script "$(realpath "$TARGET_ROOTFS_DIR")"
-done
+# for script in ./post-build.sh ../device/rockchip/common/post-build.sh; do
+# 	[ -x $script ] || continue
+# 	sudo $script "$(realpath "$TARGET_ROOTFS_DIR")"
+# done
+
+sudo ./add-build-info.sh ${TARGET_ROOTFS_DIR}
 
 dd if=/dev/zero of=${ROOTFSIMAGE} bs=1M count=0 seek=${IMAGE_SIZE_MB}
 
